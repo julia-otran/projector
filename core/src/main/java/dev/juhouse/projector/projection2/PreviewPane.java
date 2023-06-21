6a5035214e8e66da3da11c6d03f1fa0ef5acd579
Júlia Otranto Aulicino <julia.otranto@outlook.com>
2023-06-20 23:48:09 -0300

    Fix high memory allocation on previews

--- core/src/main/java/dev/juhouse/projector/projection2/PreviewPane.java
+++ core/src/main/java/dev/juhouse/projector/projection2/PreviewPane.java
@@ -5,21 +5,29 @@
  */
 package dev.juhouse.projector.projection2;
 
+import com.sun.javafx.geom.Rectangle;
+import dev.juhouse.projector.Projector;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.TitledPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelBuffer;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.WritableImage;
+import javafx.util.Callback;
 
+import java.lang.reflect.Method;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Julia Otranto Aulicino julia.otranto@outlook.com
  */
 public class PreviewPane extends TitledPane {
-    private ImageView imageView;
+    private final ImageView imageView;
 
     private ByteBuffer buffer;
     private PixelBuffer<IntBuffer> pixelBuffer;
@@ -27,6 +35,13 @@ public class PreviewPane extends TitledPane {
     private final CanvasDelegate delegate;
 
     private BridgeRender render;
+
+    private final Callback<PixelBuffer<IntBuffer>, Rectangle2D> updateImageCallback = (buffer) -> {
+        isUpdating = false;
+        return null;
+    };
+
+    private boolean isUpdating;
 
     public PreviewPane(CanvasDelegate delegate) {
         this.delegate = delegate;
@@ -41,6 +56,8 @@ public class PreviewPane extends TitledPane {
 
         setContent(imageView);
         setCollapsible(false);
+
+        isUpdating = false;
     }
 
     public void setBridgeRender(BridgeRender render) {
@@ -49,15 +66,22 @@ public class PreviewPane extends TitledPane {
         buffer = ByteBuffer.allocateDirect(render.getWidth() * render.getHeight() * 4);
         pixelBuffer = new PixelBuffer<>(render.getWidth(), render.getHeight(), buffer.asIntBuffer(), PixelFormat.getIntArgbPreInstance());
 
+        imageView.setImage(new WritableImage(pixelBuffer));
+
         setText(render.getRenderName());
     }
 
     public void downloadImage() {
+        if (isUpdating) {
+            return;
+        }
+
         delegate.getBridge().downloadPreviewData(render.getRenderId(), buffer);
     }
 
     public void updateImage() {
-        imageView.setImage(new WritableImage(pixelBuffer));
+        isUpdating = true;
+        pixelBuffer.updateBuffer(updateImageCallback);
     }
 
 }
