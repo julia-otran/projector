fc09ec212432fbc077a9eebf02e09c92058b0533
Júlia Otranto Aulicino <julia.otranto@outlook.com>
2024-01-14 04:05:19 -0300

    Try

--- core/native/render/vs-black-level-adjust.c
+++ core/native/render/vs-black-level-adjust.c
@@ -3,8 +3,45 @@
 void vs_black_level_adjust_render(config_virtual_screen *config) {
     glEnable(GL_COLOR_MATERIAL);
 
+    double max_r = 0.0d;
+    double max_g = 0.0d;
+    double max_b = 0.0d;
+
+    for (int i = 0; i < config->count_black_level_adjusts; i++) {
+        config_black_level_adjust *bla = &config->black_level_adjusts[i];
+
+        if (bla->color.r * bla->color.a > max_r) {
+            max_r = bla->color.r * bla->color.a;
+        }
+        if (bla->color.g * bla->color.a > max_g) {
+            max_g = bla->color.g * bla->color.a;
+        }
+        if (bla->color.b * bla->color.a > max_b) {
+            max_b = bla->color.b * bla->color.a;
+        }
+    }
+
+    if (max_r + max_g + max_b > 0.0) {
+        glBlendFunc(GL_ZERO, GL_ONE_MINUS_SRC_COLOR);
+
+        glColor4f(
+            max_r,
+            max_g,
+            max_b,
+            0.0);
+
+        glBegin(GL_QUADS);
+
+        glVertex2d(0.0d, 0.0d);
+        glVertex2d(0.0d, config->h);
+        glVertex2d(config->w, config->h);
+        glVertex2d(config->w, 0.0d);
+
+        glEnd();
+    }
+
     glBlendEquation(GL_MAX);
-    glBlendFunc(GL_ONE, GL_ONE);
+    glBlendFunc(GL_ONE, GL_DST_ALPHA);
     
     for (int i = 0; i < config->count_black_level_adjusts; i++) {
         config_black_level_adjust *bla = &config->black_level_adjusts[i];
@@ -13,7 +50,7 @@ void vs_black_level_adjust_render(config_virtual_screen *config) {
             bla->color.r * bla->color.a,
             bla->color.g * bla->color.a,
             bla->color.b * bla->color.a,
-            0.0);
+            1.0);
 
         glBegin(GL_QUADS);
 
